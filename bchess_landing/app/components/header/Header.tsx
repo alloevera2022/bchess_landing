c7a3f25c57f51e3840aa2f80e651b5eb6887cd1e
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-04-24 11:20:37 +0000

    create next.js

---
+++ bchess_landing/app/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import "./header.css";
+
+export default function Header() {
+    return (
+        <header className="main-header">
+            <nav className="navigation-links">
+                <h1>B-Chess</h1>
+                <h1>Play</h1>
+
+            </nav>
+        </header>
+    );
+}
+
+
+
