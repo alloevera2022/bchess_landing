757d5752c59971b9f0472bade34638529893a5e0
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-04-24 13:16:53 +0000

    add header

--- bchess_landing/app/components/header/Header.tsx
+++ bchess_landing/app/components/header/Header.tsx
@@ -5,8 +5,7 @@ export default function Header() {
         <header className="main-header">
             <nav className="navigation-links">
                 <h1>B-Chess</h1>
-                <h1>Play</h1>
-
+                <button className="button-play-header">Play</button>
             </nav>
         </header>
     );
