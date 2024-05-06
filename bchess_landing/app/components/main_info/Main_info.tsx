05b7ce1139854edcb2f64f4776b209c355020180
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-05-06 21:11:44 +0000

    try to fix build №2

--- bchess_landing/app/components/main_info/Main_info.tsx
+++ bchess_landing/app/components/main_info/Main_info.tsx
@@ -1,17 +1,15 @@
 import "./main_info.css";
-// import logo_blue from '/workspace/bchess_landing/bchess_landing/app/components/public/logo_blue.png';
 
 export default function Main_info() {
     return (
         <section className="main_info">
             <div className="main_info_content">
-                <p className="main_info_content_text">At BChess.io, we're redefining the way chess is played, combined, and enjoyed. As pioneers in the blockchain gaming space, we're excited to introduce you to the first partially onchain chess game built on the innovative Blast.io blockchain. Our commitment is to provide you with an unmatched gaming experience that's not only fun but also secure and fair.</p>
+                <p className="main_info_content_text">At BChess.io, we&apos;re redefining the way chess is played, combined, and enjoyed. As pioneers in the blockchain gaming space, we&apos;re excited to introduce you to the first partially onchain chess game built on the innovative Blast.io blockchain. Our commitment is to provide you with an unmatched gaming experience that&apos;s not only fun but also secure and fair.</p>
             </div>
-
         </section>
-
     );
 }
+
 
 
 
