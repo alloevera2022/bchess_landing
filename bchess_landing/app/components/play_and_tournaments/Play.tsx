edc4482a67c060966e6265c10e5bb044e791f325
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-05-06 21:21:37 +0000

    try to fix build №5

--- bchess_landing/app/components/play_and_tournaments/Play.tsx
+++ bchess_landing/app/components/play_and_tournaments/Play.tsx
@@ -8,7 +8,7 @@ export default function Play() {
                 <div className="play_content_block">
                     <div className="play_content_text">
                         <h1 className="play_header">Play Chess Like Never Before</h1>
-                        <h2 className="play_sub_header">Currently, BChess.io offers peer-to-peer (P2P) gaming, allowing you to challenge friends or players from around the globe to a game of chess. Whether you&apos;re looking to have fun, improve your skills, or simply enjoy the game of kings in a modern format, BChess.io is the place for you.<br /><br />
+                        <h2 className="play_sub_header">Currently, BChess.io offers peer-to-peer (P2P) gaming, allowing you to challenge friends or players from around the globe to a game of chess. Whether you are looking to have fun, improve your skills, or simply enjoy the game of kings in a modern format, BChess.io is the place for you.<br /><br />
                             Why Play on BChess.io? Decentralized Gaming: Experience chess on a platform where integrity and fairness are built into every move. Secure and Transparent: Thanks to the Blast.io blockchain, your games are secure, and every move is verifiable. Play Anytime, Anywhere: Challenge your friends or other players to a match, regardless of where you are in the world. Community and Competition: Join a growing community of chess enthusiasts and blockchain aficionados.</h2>
                     </div>
                     <div className="play_content_img">
