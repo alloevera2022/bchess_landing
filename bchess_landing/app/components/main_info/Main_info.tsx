0649be1c29fa678cd550f0bec08deb8d4672b00f
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-05-21 20:28:23 +0000

    fix deploy

--- bchess_landing/app/components/main_info/Main_info.tsx
+++ bchess_landing/app/components/main_info/Main_info.tsx
@@ -4,7 +4,7 @@ export default function Main_info() {
     return (
         <section className="main_info">
             <div className="main_info_content">
-                <p className="main_info_content_text">At BChess, we're redefining the way chess is played, combined, and enjoyed. As pioneers in the blockchain gaming space, we're excited to introduce you to the first partially onchain chess game built on the innovative Blast blockchain. Our commitment is to provide you with an unmatched gaming experience that's not only fun but also secure and fair.</p>
+                <p className="main_info_content_text">At BChess, we&apos;re redefining the way chess is played, combined, and enjoyed. As pioneers in the blockchain gaming space, we&apos;re excited to introduce you to the first partially onchain chess game built on the innovative Blast blockchain. Our commitment is to provide you with an unmatched gaming experience that&apos;s not only fun but also secure and fair.</p>
             </div>
         </section>
     );
